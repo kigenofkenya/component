4e8f120281883b1af858a34124b4c02e3dc21339
Coffcer <coffce404@gmail.com>
2016-04-09 20:33:49 +0800

    update

--- template/dev/index.js
+++ template/dev/index.js
@@ -3,10 +3,5 @@ import Component from '../src/index.vue'
 
 new Vue({
   el: '#app',
-  data: {
-      title: '{{ name }}',
-      desc: '{{ desc }}',
-      github: 'https://github.com/{{ author }}/{{ name }}',
-  },
   components: { app: Component },
 })
